b76ae3bba9b8b013e3389e5e5a61ed2bbeadc9a9
phonnaphaW <phonnphap@gmail.comW>
2026-06-01 20:49:32 +0700

    Clean up config and middleware runtime

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ลบส่วน experimental ออกทั้งหมดตรงนี้
   webpack: (config, { isServer }) => {
-    // ถ้าไม่ได้เป็นการรันฝั่ง Server (เป็นฝั่ง Client หน้าบ้าน)
     if (!isServer) {
-      // สั่งให้เปลี่ยนตัวแปรระบบเหล่านี้เป็นค่าว่าง เพื่อไม่ให้ face-api ฟ้องเออเร่อ
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
