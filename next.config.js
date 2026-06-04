5b827745b112afdff20ba2fa2a648519b3d7d643
phonnaphaW <phonnphap@gmail.comW>
2026-06-05 02:01:15 +0700

    fix: use experimental.serverComponentsExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['face-api.js'],
+  experimental: {
+    serverComponentsExternalPackages: ['face-api.js'],
+  },
 
   webpack: (config, { isServer }) => {
-    // ไม่ให้ face-api.js เข้า bundle ฝั่ง Edge/Middleware เลย
     config.externals = config.externals || [];
     if (Array.isArray(config.externals)) {
       config.externals.push('face-api.js');
