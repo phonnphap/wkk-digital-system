a6fd54434bda41c6b467cb97ff8bb44dc1c001f5
phonnaphaW <phonnphap@gmail.comW>
2026-06-05 11:20:40 +0700

    fix: next15 config with turbopack

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['face-api.js'],
-  },
-
-  webpack: (config, { isServer }) => {
-    config.externals = config.externals || [];
-    if (Array.isArray(config.externals)) {
-      config.externals.push('face-api.js');
-    }
-
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        crypto: false,
-        os: false,
-        stream: false,
-        buffer: false,
-      };
-    }
-    return config;
-  },
+  serverExternalPackages: ['face-api.js'],
+  turbopack: {},
 };
 
 module.exports = nextConfig;
