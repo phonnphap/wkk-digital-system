5e92a6672215b8f6a2972f05a88e91263cb823b4
phonnaphaW <phonnphap@gmail.comW>
2026-06-07 12:27:17 +0700

    d=edit sub

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "ระบบสแกนใบหน้า โรงเรียนวัดเขียนเขต",
+  title: "ระบบปฏิบัติงาน โรงเรียนวัดเขียนเขต",
   description: "ระบบลงเวลาทำงานและจัดการข้อมูลโรงเรียนวัดเขียนเขต",
 };
 
